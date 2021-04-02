57e721cce5c4a983c951d1b9860ab94b9df96263
jiannan.chen <jiannan.chen@corp.to8to.com>
2021-04-02 13:53:41 +0800

    add some sort algorithm

--- C++/SortAlgorithm.cpp
+++ C++/SortAlgorithm.cpp
@@ -4,7 +4,7 @@
 */
 int* BubbleSort(int *arr, int len){
 
-	if(!arr or len <= 1 )return nullptr;
+	if(!arr || len <= 1 )return nullptr;
 
 	for(int i = 0;i < len - 1;i++){
 		for(int j = 0;j < len - i - 1;j++){ //注意len-i-1
@@ -24,7 +24,7 @@ int* BubbleSort(int *arr, int len){
 */
 int* SelectSort(int *arr, int len){
 
-	if(!arr or len <= 1 )return nullptr;
+	if(!arr || len <= 1 )return nullptr;
 
 	for (int i = 0;i < len - 1;i++){
 		int min = i;
@@ -48,11 +48,12 @@ int* SelectSort(int *arr, int len){
 */
 int* InsertSort(int *arr, int len){
 
-	if(!arr or len <= 1 )return nullptr;
+	if(!arr || len <= 1 )return nullptr;
 
 	for (int i = 1;i < len;i++){
 		int tmp = arr[i];
-		for (int j = i - 1;j >= 0 && arr[j] > tmp;j--){
+		int j = i - 1;
+		for (;j >= 0 && arr[j] > tmp;j--){
 			arr[j + 1] = arr[j];
 		}
 		arr[j + 1] = tmp;
@@ -66,12 +67,13 @@ int* InsertSort(int *arr, int len){
 
 int* ShellSort(int *arr, int len){
 
-	if(!arr or len <= 1 )return nullptr;
+	if(!arr || len <= 1 )return nullptr;
 
 	for (int gap = len/2; gap > 0; gap = gap/2){
 		for (int i = gap;i < len;i++){
 			int tmp = arr[i];
-			for(int j = i - gap; j>= 0 && arr[j] > tmp; j = j - gap){
+			int j = i - gap;
+			for(; j>= 0 && arr[j] > tmp; j = j - gap){
 					arr[j + gap] = arr[j];
 			}
 			arr[j + 1] = tmp;
@@ -87,7 +89,7 @@ int* ShellSort(int *arr, int len){
 */
 void AdjustHeap(int *arr, int i, int len){
 
-	if(!arr or len <=1)return;
+	if(!arr || len <=1)return;
 
 	int tmp = arr[i];
 	for (int k = 2 * i + 1; k < len; k = 2 * k + 1)
@@ -111,7 +113,7 @@ void AdjustHeap(int *arr, int i, int len){
  
 int* HeapSort(int *arr,int len)
 {
-	if (!arr or len <= 1)return nullptr;
+	if (!arr || len <= 1)return nullptr;
 
 	for (int i = len / 2 - 1;i >= 0 ;i--)
 	{
@@ -140,7 +142,7 @@ void Swap(int *arr, int a, int b){
  */
 int* MergeSort(int *arr, int len){
 
-    if (!arr or len <= 1)return nullptr;
+    if (!arr || len <= 1)return nullptr;
 
     int *tmp = new int[len];//在排序前，先建好一个长度等于原数组长度的临时数组，避免递归中频繁开辟空间
     sort(arr, 0, len-1, tmp);
@@ -177,3 +179,83 @@ void merge(int* arr, int sta, int mid, int end, int* tmp){
         arr[sta++] = tmp[t++];
     }
 }
+
+
+/*
+ * 基数排序
+ * @parameter
+ * dec: 数据位数
+ * order: 正在排序的位数，1代表个位，2代表十位，3代表百位
+ */
+#include <stdio.h>
+#include <string.h>
+
+/* 获取输入数字的索引值,dec指定数字的位数,3代表百位数,order指定需要获取哪一位的索引,1代表个位,2代表十位,3代表百位 */
+int get_index(int num, int dec, int order)
+{
+    int i, j, n;
+    int index;
+    int div;
+
+    /* 根据位数,循环减去不需要的高位数字 */
+    for (i=dec; i>order; i--) {
+        n = 1;
+        for (j=0; j<dec-1; j++)
+            n *= 10;
+        div = num/n;
+        num -= div * n;
+        dec--;
+    }
+
+    /* 获得对应位数的整数 */
+    n = 1;
+    for (i=0; i<order-1; i++)
+        n *= 10;
+
+    /* 获取index */
+    index = num / n;
+
+    return index;
+}
+
+void radix_sort(int* arr, int len, int dec, int order)
+{
+    int i, j;
+    int index;     /* 排序索引 */
+    int *tmp = new int[len];  /* 临时数组,用来保存待排序的中间结果 */
+    int *num = new int[10];   /* 保存索引值的数组 */
+    memset(num, 0, 10*sizeof(int));  /* 数组初始清零 */
+    memset(tmp, 0, len*sizeof(int)); /* 数组初始清零 */
+
+    if (dec < order) /* 最高位排序完成后返回 */
+        return;
+
+    for (i=0; i<len; i++) {
+        index = get_index(arr[i], dec, order);  /* 获取索引值 */
+        num[index]++;  /* 对应位加一 */
+    }
+
+    for (i=1; i<10; i++)
+        num[i] += num[i-1]; /* 调整索引数组 */
+
+    for (i=len-1; i>=0; i--) {
+        index = get_index(arr[i], dec, order);  /* 从数组尾开始依次获得各个数字的索引 */
+        j = --num[index];  /* 根据索引计算该数字在按位排序之后在数组中的位置 */
+        tmp[j] = arr[i]; /* 数字放入临时数组 */
+    }
+
+    for (i=0; i<len; i++)
+        arr[i] = tmp[i];  /* 从临时数组复制到原数组 */
+
+    printf("the %d time\n", order);
+    for (i=0; i<30; i++)
+        printf("%d  ", arr[i]);
+    printf("\n");
+
+    /* 继续按高一位的数字大小进行排序 */
+    radix_sort(arr, len, dec, order+1);
+
+    return;
+}
+
+
